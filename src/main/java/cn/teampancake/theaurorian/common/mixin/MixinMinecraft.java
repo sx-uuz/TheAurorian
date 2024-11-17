a643b2e7b9acaed0627050835959cd604fcb4042
glyceryl <494192175@qq.com>
2024-11-17 23:02:26 +0800

    Resolve an issue of excessive memory usage.

--- src/main/java/cn/teampancake/theaurorian/common/mixin/MixinMinecraft.java
+++ src/main/java/cn/teampancake/theaurorian/common/mixin/MixinMinecraft.java
@@ -1,11 +1,17 @@
 package cn.teampancake.theaurorian.common.mixin;
 
 import cn.teampancake.theaurorian.common.registry.TAEnchantments;
+import cn.teampancake.theaurorian.common.utils.EnchantmentUtils;
+import com.google.common.collect.Maps;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.multiplayer.ClientLevel;
 import net.minecraft.client.player.LocalPlayer;
 import net.minecraft.core.Holder;
 import net.minecraft.world.entity.Entity;
+import net.minecraft.world.entity.EquipmentSlot;
+import net.minecraft.world.entity.EquipmentSlotGroup;
+import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.enchantment.Enchantment;
 import net.minecraft.world.item.enchantment.EnchantmentHelper;
 import org.spongepowered.asm.mixin.Mixin;
@@ -15,6 +21,8 @@ import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 import javax.annotation.Nullable;
+import java.util.List;
+import java.util.Map;
 
 @Mixin(Minecraft.class)
 public class MixinMinecraft {
@@ -27,7 +35,7 @@ public class MixinMinecraft {
         if (this.player != null && Minecraft.getInstance().getCameraEntity() == this.player) {
             Holder<Enchantment> holder = TAEnchantments.get(this.player.level(), TAEnchantments.MOONLIGHT);
             double distance = this.player.distanceToSqr(entity);
-            int i = EnchantmentHelper.getEnchantmentLevel(holder, this.player);
+            int i = EnchantmentUtils.getEnchantmentLevel(holder, this.player);
             if (i > 0 && distance <= Math.pow(i * 20.0D, 2.0D)) {
                 cir.setReturnValue(true);
             }
